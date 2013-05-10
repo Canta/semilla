ca043bb9c0bfdb08af992e816f5f26b4f08c8e89
Daniel Cantarín <omega_canta@yahoo.com>
2013-05-10 15:40:05 -0300

    added a function for html conversion to text, that handles new lines better than jQuery's .text().

--- templates/default/js/00-common.js
+++ templates/default/js/00-common.js
@@ -230,6 +230,41 @@ function convertDataURIToBinary(dataURI) {
   return array;
 }
 
+function convertHtmlToText(inputText) {
+    var returnText = "" + inputText;
+
+    //-- remove BR tags and replace them with line break
+    returnText=returnText.replace(/<br>/gi, "\n");
+    returnText=returnText.replace(/<br\s\/>/gi, "\n");
+    returnText=returnText.replace(/<br\/>/gi, "\n");
+
+    //-- remove P and A tags but preserve what's inside of them
+    returnText=returnText.replace(/<p.*>/gi, "\n");
+    returnText=returnText.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
+
+    //-- remove all inside SCRIPT and STYLE tags
+    returnText=returnText.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
+    returnText=returnText.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
+    //-- remove all else
+    returnText=returnText.replace(/<(?:.|\s)*?>/g, "");
+
+    //-- get rid of more than 2 multiple line breaks:
+    returnText=returnText.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "\n\n");
+
+    //-- get rid of more than 2 spaces:
+    returnText = returnText.replace(/ +(?= )/g,'');
+
+    //-- get rid of html-encoded characters:
+    returnText=returnText.replace(/&nbsp;/gi," ");
+    returnText=returnText.replace(/&amp;/gi,"&");
+    returnText=returnText.replace(/&quot;/gi,'"');
+    returnText=returnText.replace(/&lt;/gi,'<');
+    returnText=returnText.replace(/&gt;/gi,'>');
+
+    //-- return
+    return returnText;
+}
+
 
 /**
  * app.contents.edition.edit
@@ -388,7 +423,8 @@ app.contents.edition.save_fragment = function(){
 	var f1  = c.fragments[app.contents.edition.current_fragment];
 	var f2  = new Semilla.Fragment();
 	f2.content = ""; //not needed for text edition.
-	f2.text    = $("#fragment-editor").text();
+	//f2.text    = $("#fragment-editor").text();
+	f2.text    = convertHtmlToText($("#fragment-editor").html());
 	f2.html    = $("#fragment-editor").html();
 	f2.from    = f1.from;
 	f2.to      = f1.to;
